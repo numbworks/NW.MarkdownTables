dee72331c172d3bd322dee6ec491262ea3f7edaa
Rubèn <numbworks@gmail.com>
2020-10-11 20:45:52 +0200

    OutputOptions, removed accordingly.

--- src/NW.MarkdownTabulizer/Messages/MessageCollection.cs
+++ src/NW.MarkdownTabulizer/Messages/MessageCollection.cs
@@ -9,8 +9,6 @@ namespace NW.MarkdownTabulizer
         public static Func<string, string> CantHaveZeroItems = (name) => $"{name} can't have zero items.";
         public static Func<NullHandlingStrategies, string> ProvidedNullHandlingStrategyNotValid =
             (strategy) => $"The provided '{nameof(NullHandlingStrategies)}' strategy is not valid ('{strategy.ToString()}').";
-        public static Func<OutputOptions, string> ProvidedOutputOptionNotValid =
-            (option) => $"The provided '{nameof(OutputOptions)}' option is not valid ('{option.ToString()}').";
 
     }
 }
