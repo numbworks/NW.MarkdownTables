e70a3688b59ea37d9122af47b5149476a9120f06
Rubèn <numbworks@gmail.com>
2021-10-11 15:41:51 +0200

    NullHandlingStrategies: added summaries.

--- src/NW.MarkdownTables/Strategies/NullHandlingStrategies.cs
+++ src/NW.MarkdownTables/Strategies/NullHandlingStrategies.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace NW.MarkdownTables.Strategies
 {
+    /// <summary>Collects all the null handling strategies.</summary>
     public enum NullHandlingStrategies
     {
 
